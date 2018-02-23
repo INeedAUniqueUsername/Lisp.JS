fc1195e450917752068b14962f5b3bada6edae7f
INeedAUniqueUsername <achen115@ucr.edu>
2018-02-22 23:12:07 -0800

    no more functions

--- Lisp.js
+++ Lisp.js
@@ -1,7 +1,11 @@
+var code;
 //var code = "(block (result1 result2) (set result1 (add 1 5)) (set result2 (add 5 6)))";
 //var code = "(divide (add (multiply 5 21) (divide 1 10) (add 1) 2 (subtract 0 10)) 100)";
 //code = '(add (set a 1000) a (multiply a a))';
-code = '\'aaaa(a b c)';
+//code = '(list (add 1 2 3) (list 1 "a" 3) 2 "a")';
+//code = '(at (append (list 1 2 3) (list 1 2 3 4)) 0)';
+//code = '(if false (power 2 2 2 2) 5)';
+code = '(or 1 2 (and 1 2 3 0))'
 var indent = function(level) {
     var result = '';
     for(var i = 0; i < level; i++)  {
@@ -18,17 +22,10 @@ var parse = function(codeString) {
     let parsingLiteral = false;
     let parsingLiteralType = '';
     let literalListParenCount = 0;
-    let startLiteralList = false;
     
     for(let i = 0; i < codeString.length; ++i) {
         let c = codeString.charAt(i);
         let layer = layers[layers.length-1];
-        
-        if(startLiteralList) {
-            startLiteralList = false;
-            parsingLiteral = true;
-            parsingLiteralType = 'list';
-        }
         
         if(parsingLiteral) {
             if(c === ' ') {
@@ -49,16 +46,23 @@ var parse = function(codeString) {
                     parsingLiteral = false;
                 }
             } else if(c === '(') {
+                //If we just started parsing this literal and the first char we find after the apostrophe is am open parenthesis, then we are looking at a list.
                 if(!parsingLiteralType) {
                     parsingLiteralType = 'list';
                 }
                 
+                //If we're already parsing a literal list structure, then we found a nested list structure.
                 if(parsingLiteralType === 'list') {
                     layer[layer.length-1] += c;
                 } else {
                     //Immediately transition to parsing a new list literal
-                    layers[layers.length-1].push(c);
+                    //layers[layers.length-1].push(c);
                     parsingLiteralType = 'list';
+                    layers[layers.length-1].push('\'');
+                    i--;
+                    
+                    //Prepare the string for use
+                    layer[layer.length-1] = '"' + layer[layer.length-1].substring(1) + '"';
                 }
                 literalListParenCount++;
             } else {
@@ -148,64 +152,188 @@ var decode = function(code) {
         return result;
     }
 }
-var decodeFirst = function(code) {
-    if(!Array.isArray(code)) {
-        return code;
-    } else if(code.length === 0) {
-        return '()';
-    } else if(code.length === 1) {
-        return '(' + code[0] + ')';
-    } else {
-        var result = '(' + decode(code[0]);
-        for(var i = 1; i < code.length; i++) {
-            result += ' ' + run(code[i]);
-        }
-        result += ')';
-        return result;
-    }
-}
 var globals = {
     add: function(args) {
         let sum = 0;
         for(let i = 0; i < args.length; i++) {
-            sum += parseFloat(run(args[i]));
+            sum += parseFloat(eval(args[i]));
         }
         return sum;
     },
     subtract: function(args) {
-        let result = parseFloat(run(args[0]));
+        let result = parseFloat(eval(args[0]));
         for(let i = 1; i < args.length; i++) {
-            result -= parseFloat(run(args[i]));
+            result -= parseFloat(eval(args[i]));
         }
         return result;
     },
     multiply: function(args) {
-        let result = parseFloat(run(args[0]));
+        let result = parseFloat(eval(args[0]));
         for(let i = 1; i < args.length; i++) {
-            result *= parseFloat(run(args[i]));
+            result *= parseFloat(eval(args[i]));
         }
         return result;
     },
     divide: function(args) {
-        let result = parseFloat(run(args[0]));
+        let result = parseFloat(eval(args[0]));
         for(let i = 1; i < args.length; i++) {
-            result /= parseFloat(run(args[i]));
+            result /= parseFloat(eval(args[i]));
         }
         return result;
+    },
+    //Takes arguments base, e1, e2, e3, ... where result = (((base^e1)^e2)^e3)^e...
+    power: function(args) {
+        let base = eval(args[0]);
+        let result = 1;
+        for(let i = 1; i < args.length; i++) {
+            let exponent = eval(args[i]);
+            for(let j = 0; j < exponent; j++) {
+                result *= base;
+            }
+            base = result;
+            result = 1;
+        }
+        return base;
     },
     set: function(args) {
         if(args.length < 2) {
             return 'Missing arguments';
         } else {
-            return (globals[args[0]] = run(args[1]));
+            return (globals[args[0]] = eval(args[1]));
         }
     },
     cat: function(args) {
         let result = '';
         for(let i = 0; i < args.length; i++) {
-            result += destring(run(args[i]));
+            result += destring(eval(args[i]));
         }
         return '"' + result + '"';
+    },
+    list: function(args) {
+        let result = [];
+        for(let i = 0; i < args.length; i++) {
+            result.push(eval(args[i]));
+        }
+        return result;
+    },
+    append: function(args) {
+        let result = [];
+        for(let i = 0; i < args.length; i++) {
+            let list = eval(args[i]);
+            for(let j = 0; j < list.length; j++) {
+                result.push(list[j]);
+            }
+        }
+        return result;
+    },
+    at: function(args) {
+        let list = eval(args[0]);
+        let index = eval(args[1]);
+        //console.log("List: " + list);
+        //console.log("Index: " + index);
+        return list[index];
+    },
+    if: function(args) {
+        let condition = eval(args[0]);
+        let path_true = args[1];
+        let path_false = args[2];
+        return truth(condition) ? eval(path_true) : eval(path_false);
+    },
+    and: function(args) {
+        let result;
+        for(let i = 0; i < args.length; i++) {
+            if(!truth(result = eval(args[i]))) {
+                return false;
+            }
+        }
+        return result;
+    },
+    or: function(args) {
+        let result;
+        for(var i = 0; i < args.length; i++) {
+            if(truth(result = eval(args[i]))) {
+                return result;
+            }
+        }
+        return false;
+    },
+    //(xor condition1 condition2 condition3 ...) -> true if all expressions are neither all true nor all false
+    xor: function(args) {
+        let truthValue = 0;
+        for(var i = 0; i < args.length; i++) {
+            if(truth(eval(args[i]))) {
+                truthValue++;
+            } else {
+                truthValue--;
+            }
+        }
+        if(Math.abs(truthValue) === args.length) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    not: function(args) {
+        return !truth(eval(args[0]));
+    },
+    nand: function(args) {
+        return !globals.and(args);
+    },
+    nor: function(args) {
+        return !globals.or(args);
+    },
+    xnor: function(args) {
+        return !globals.xor(args);
+    },
+    //(branch condition1 path1 condition2 path2 condition 3 path3 ... [default])
+    //If condition1 is true, then eval path1
+    //Else if condition2 is true, then eval path2
+    //Else if condition3 is true, then eval path3
+    //...
+    //Else run default
+    branch: function(args) {
+        for(let i = 0; i+1 < args.length; i++) {
+            if(eval(args[i])) {
+                return eval(args[i+1]);
+            }
+        }
+        //Odd number of arguments means there's a default path
+        if(args.length%2 == 1) {
+            return eval(args[args.length-1]);
+        }
+    },
+    //(switch value case1 path1 case2 path2 case3 path3 ... [default]) -> result of path
+    //If value === case1, then eval path1
+    //Else if value === case2, then eval path2
+    //Else if value === case 3, then eval path3
+    //Else eval default
+    switch: function(args) {
+        let value = eval(args[0]);
+        for(let i = 1; i < args.length; i++) {
+            if(value === eval(args[i])) {
+                return eval(args[i+1]);
+            }
+        }
+        //Even number of arguments means there's a default path
+        if(args.length%2 == 0) {
+            return eval(args[args.length-1]);
+        }
+    },
+    //(race path1 path2 path3 ... pathn)
+    //Evaluates all expressions in random order, race condition guaranteed
+    race: function(args) {
+        let result;
+        for(let i = args.length-1; i > -1; i--) {
+            let roll = Math.floor(Math.random() * i);
+            [args[i], a[roll]] = [a[roll], a[i]];
+            result = eval(args[i]);
+        }
+        return result;
+    },
+    //(diverge path1 path2 path3 ... pathn)
+    //Diverges the timeline towards one of the paths
+    diverge: function(args) {
+        return eval(args[Math.floor(Math.random * args.length)]);
     },
     block: function(args) {
         let locals = args[0];
@@ -219,7 +347,7 @@ var globals = {
                     }
                     let name = local[0];
                     previousGlobals[name] = globals[name];
-                    globals[name] = run(locals[1]);
+                    globals[name] = eval(locals[1]);
                 } else {
                     previousGlobals[local] = globals[local];
                     delete globals[local];
@@ -230,14 +358,63 @@ var globals = {
         }
         let result = '';
         for(let i = 1; i < args.length; i++) {
-            result = run(args[i]);
+            result = eval(args[i]);
         }
         for(let previousGlobal in previousGlobals) {
             globals[previousGlobal] = previousGlobals[previousGlobal];
         }
         return result;
+    },
+    eval: function(code) {
+        if(!Array.isArray(code)) {
+            if(code === 'true') {
+                return true;
+            } else if(code === 'false') {
+                return false;
+            } else if(code.startsWith('"') && code.endsWith('"')) {
+                //Ignore strings
+                return code;
+            }
+            let result;
+            if((result = parseFloat(code) || parseInt(code)) || result === 0) {
+                return result;
+            }
+            //May be looking at a global
+            result = globals[code];
+            if(result) {
+                console.log(code + ' -> ' + result);
+                return result;
+            }
+            return 'no binding for symbol [' + code + '] ### ' + decode(code) + ' ### ';
+        } else {
+            let command = code[0];
+            let args = [];
+            for(var i = 1; i < code.length; i++) {
+                args.push(code[i]);
+            }
+            
+            //See if we're calling a function with our first argument
+            let func = globals[command];
+            if(typeof func === 'function') {
+                try {
+                    let result = func(args);
+                    console.log(decode(code) + ' -> ' + result);
+                    return result;
+                } catch(error) {
+                    return error + ' ### ' + decode(code) + ' ###';
+                }
+                
+            } else if(typeof func !== 'undefined') {
+                return 'not a function [' + command + '] ### ' + decode(code) + ' ### ';
+            } else {
+                return 'no binding for symbol [' + command + '] ### ' + decode(code) + ' ### ';
+            }
+        }
     }
 }
+let truth = function(condition) {
+    return condition || condition === 0;
+};
 var isString = function(string) {
     return typeof string === 'string' && string.startsWith('"') && string.endsWith('"');
 }
@@ -253,44 +430,9 @@ var destring = function(string) {
     }
     return string;
 }
-var run = function(code) {
-    if(!Array.isArray(code)) {
-        //Ignore strings
-        if(code.startsWith('"') && code.endsWith('"')) {
-            return code;
-        }
-        
-        //May be looking at a global
-        let result = globals[code];
-        if(result) {
-            console.log(code + ' -> ' + result);
-            return result;
-        } else {
-            return code;
-        }
-    } else {
-        let command = code[0];
-        let args = [];
-        for(var i = 1; i < code.length; i++) {
-            args.push(code[i]);
-        }
-        let func = globals[command];
-        if(typeof func === 'function') {
-            try {
-                let result = func(args);
-                console.log(decode(code) + ' -> ' + result);
-                return result;
-            } catch(error) {
-                return error + ' ### ' + decode(code) + ' ###';
-            }
-            
-        } else if(typeof func !== 'undefined') {
-            return 'not a function [' + command + '] ### ' + decode(code) + ' ###';
-        } else {
-            return 'no binding for symbol [' + command + '] ### ' + decode(code) + ' ###';
-        }
-    }
+var eval = function(code) {
+    return globals.eval(code);
 }
 
-console.log(run(parse(code)));
+console.log(eval(parse(code)));
 'done'
